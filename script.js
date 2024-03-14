345d2b5071b3fa3acb94a94932b057cca26dfef7
DajuanA15 <143430633+DajuanA15@users.noreply.github.com>
2024-03-14 15:10:56 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let input1 = document.querySelector (".question1")
+let input2 = document.querySelector (".question2")
+let button = document.querySelector(".submit")
+
+if(answer === "red and yellow" && answer2 === "speed")(
+console.log("flash")
+)
+
+if(answer === "blue and red" && answer2 === "Everything")(
+    console.log("superman")
+    )
+
+    if(answer === "green and purple" && answer2 === "intellegence")(
+      console.log("lex")
+    )
